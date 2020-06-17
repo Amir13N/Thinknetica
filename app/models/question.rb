7f975a842ab0dc69813e0e856e1cd6ece24abdaa
Amir13N <meepopopo22@mail.ru>
2020-06-17 15:14:45 +0600

    tdd

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,6 @@
 class Question < ApplicationRecord
-
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :body, :title, presence: true
   validates :body, uniqueness: { scope: :title }
-
 end
