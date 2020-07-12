03c6d941b35f81d95139c716ddc4c202751de940
Amir13N <meepopopo22@mail.ru>
2020-07-12 09:42:39 +0600

    Ajax

--- db/migrate/20200625215553_add_author_reference_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorReferenceToQuestion < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :questions, :author, foreign_key: true
-  end
-end
--- db/migrate/20200625220656_add_author_foreign_key_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorForeignKeyToQuestion < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :questions, :authors
-  end
-end
