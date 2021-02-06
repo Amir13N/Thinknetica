12807d04d690113f2b9daff5b668460671a21f17
Amir13N <meepopopo22@mail.ru>
2021-02-06 22:21:29 +0600

    tests for AnswerNotifyJob

--- spec/jobs/answer_notify_job_spec.rb
+++ spec/jobs/answer_notify_job_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AnswerNotifyJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:service) { double('AnswerNotifyService') }
+  let(:answer) { create(:answer) }
+
+  before { allow(AnswerNotifyService).to receive(:new).and_return(service) }
+
+  it 'calls DailyDigestService#send_digest' do
+    expect(service).to receive(:notify)
+    AnswerNotifyJob.perform_now(answer)
+  end
 end
