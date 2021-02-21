e68bc5b4230f7a8be9b69e27f859d6e0f3db4f2a
Amir13N <meepopopo22@mail.ru>
2021-02-22 00:39:50 +0600

    Action cable deploy

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,6 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
+App.cable = ActionCable.createConsumer("/cable")
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
