cc2b0f7b4f5ab9a40292ee48a2f50abcde896a66
Amir13N <meepopopo22@mail.ru>
2021-02-24 00:03:00 +0600

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV['SMTP_USERNAM'],
-    password: ENV['SMTP_PASSWORD'],
+    user_name: 'broomygod@gmail.com',
+    password: 'greedisgood22',
     authentication: 'plain',
     enable_starttls_auto: true
   }
